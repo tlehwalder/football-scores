cf2f631d8b1e1ddb37524772407ea9dbd2f0ba84
Tobias Lehwalder <tlehwalder@posteo.de>
2020-05-25 20:55:03 +0200

    add google fonts nunito

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,15 +4,19 @@ import Header from "../components/Header";
 import MatchList from "../components/MatchList";
 import Main from "../layout/Main";
 
-const Scores = props => {
+const Scores = (props) => {
   return (
     <Main>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito&display=swap"
+        rel="stylesheet"
+      />
       <Header />
       <MatchList matches={props.matches} />
       <style jsx global>{`
         body {
           background-color: ghostwhite;
-          font-family: sans;
+          font-family: "Nunito", sans-serif;
         }
       `}</style>
     </Main>
@@ -21,11 +25,11 @@ const Scores = props => {
 
 export default Scores;
 
-Scores.getInitialProps = async function() {
+Scores.getInitialProps = async function () {
   const res = await fetch("https://www.openligadb.de/api/getmatchdata/bl1");
   const data = await res.json();
 
   return {
-    matches: data
+    matches: data,
   };
 };
