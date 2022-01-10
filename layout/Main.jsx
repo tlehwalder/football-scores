9bf5edc63c904f13140f032fa936477243edad64
tlehwalder <tobias.lehwalder@dertouristik.com>
2022-01-10 15:42:39 +0100

    feat: remove startcase

--- layout/Main.jsx
+++ layout/Main.jsx
@@ -1,6 +1,5 @@
 import {useContext, useState} from "preact/hooks";
 import ThemeContext, { THEME_LIGHT, THEME_DARK } from "../context/ThemeContext";
-import startCase from "lodash/startCase";
 import { parseCookies, setCookie } from "nookies";
 
 const Main = ({ children }) => {
@@ -26,7 +25,7 @@ const Main = ({ children }) => {
         rel="stylesheet"
       />
       <ThemeContext.Provider value={themeToggleState}>
-        <span onClick={toggleTheme}>{startCase(themeToggleState === THEME_DARK ? THEME_LIGHT : THEME_DARK)}</span>
+        <span onClick={toggleTheme}>{themeToggleState === THEME_DARK ? THEME_LIGHT : THEME_DARK}</span>
         <div className="main">{children}</div>
       </ThemeContext.Provider>
       <style jsx global>{`
